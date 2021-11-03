2c16fc7fcbc3c82d83ea8517586f6ffa8e8f3ff2
Nick Emmerson <nickemmerson619@hotmail.com>
2021-11-03 09:19:56 +0000

    Further reduction

--- src/MathExam/Difficulty.js
+++
@@ -1,3 +0,0 @@
-const getDifficulty = streak => 2 + Math.floor(streak / 3);
-
-export default getDifficulty;
--- src/MathExam/TimerStates.js
+++
@@ -1,8 +0,0 @@
-const timerStates = {
-    STATE_START: {id: 1, timeoutSecs: 10, description: 'start'},
-    STATE_PLAY: {id: 2, timeoutSecs: 10, description: 'play'},
-    STATE_PAUSE: {id: 3, timeoutSecs: 5, description: 'pause'},
-    STATE_CORRECT: {id: 4, timeoutSecs: 3, description: 'correct'}
-};
-
-export default timerStates;
