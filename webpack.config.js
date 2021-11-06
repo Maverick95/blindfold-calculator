de2ad50374b24056df8d576acff0722cbdd76dcd
Nicholas Charles Emmerson <nickemmerson619@hotmail.com>
2021-11-06 19:39:09 +0000

    Adding webpack-dev-server for running locally and using Cypress

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,15 @@
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
+    devServer: {
+        static: {
+          directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 9000,
+    },
     module: {
         rules: [
             {
