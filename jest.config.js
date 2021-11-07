5b51ae5ec7c60e731b0af5161a6428ed9c2c549f
Nicholas Charles Emmerson <nickemmerson619@hotmail.com>
2021-11-07 15:38:29 +0000

    Setup running of react in jest

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
     roots: ["<rootDir>/src"],
     testEnvironment: 'jsdom',
+    moduleNameMapper: {
+        "\\.css$": "<rootDir>/__mocks__/styleMock.js"
+      }
 };
--- src/Components/Output/Output.tsx
+++ src/Components/Output/Output.tsx
@@ -1,38 +1,42 @@
 import React from 'react';
 import './Output.css';
+import { transformEquationToEnglish } from '../../Helpers/Helpers';
 
-const transformEquationToEnglish = x => {
-    var x_return = x.replaceAll('+', 'Plus');
-    x_return = x_return.replaceAll('x', 'Multiplied By');
-    x_return = x_return.replaceAll('(', 'Open Bracket');
-    x_return = x_return.replaceAll(')', 'Close Bracket');
-    return x_return;
+interface IProps {
+    output: string,
+    valid: boolean,
+    value: number,
+    reset: () => void,
 };
 
-const OutputComponent = (props) => {
+const OutputComponent: React.FC<IProps> = ({ output, valid, value, reset }) => {
 
-    const sectionAriaLabel = `Results${
-        props.valid ? '' : ', There Is Currently No Result'}`;
+    const sectionHeaderResult = `Results${valid ? '' : ', There Is Currently No Result'}`;
+    const outputEquation = output || 'No input received';
+    const outputEquationEnglish = output ? transformEquationToEnglish(output) : 'No input received';
+    const outputValue = valid ? value : 'No valid value';
 
     return (
-      <div className="calculator-output">
-        <section aria-labelledBy="results">
-            <h2 id="results" aria-label={sectionAriaLabel} className="section-header">Results</h2>
-                <div className="calculator-output-row">
-                    <h3>Read Your Input with Maths Symbols</h3>
-                    <p id="output-equation" className="calc-output">{props.output || 'No input received'}</p>
-                </div>
-                <div className="calculator-output-row">
-                    <h3>Read Your Input in English</h3>
-                    <p id="output-english" className="calc-output">{props.output ? transformEquationToEnglish(props.output) : 'No input received'}</p>
-                </div>
-                <div className="calculator-output-row">
+        <div className="calculator-output">
+            <h2 className="section-header">Results</h2>
+            <section aria-labelledby="section-header-input-symbols" className="calculator-output-row">
+                <h3 id="section-header-input-symbols">Read Your Input with Maths Symbols</h3>
+                <p className="calc-output">{outputEquation}</p>
+            </section>
+            <section aria-labelledby="section-header-input-english" className="calculator-output-row">
+                <h3 id="section-header-input-english">Read Your Input in English</h3>
+                <p className="calc-output">{outputEquationEnglish}</p>
+            </section>
+            <div className="calculator-output-row">
+                <section aria-label={sectionHeaderResult} style={{ 'flex': '1 1 0', display: 'flex' }}>
                     <h3>Result</h3>
-                    <p id="output-result" className="calc-output">{props.valid ? props.value : 'No valid value'}</p>
-                    <h3>Reset</h3>
-                    <input id="output-reset" type="button" onClick={props.reset} value="Click Here to Reset" />
-                </div>
-        </section>
+                    <p className="calc-output">{outputValue}</p>
+                </section>
+                <section aria-labelledby="section-header-reset" style={{ 'flex': '1 1 0', display: 'flex' }}>
+                    <h3 id="section-header-reset">Reset</h3>
+                    <input type="button" onClick={reset} value="Click Here to Reset" />
+                </section>
+            </div>
         </div>
     );
 
