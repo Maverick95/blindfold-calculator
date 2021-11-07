38e75cad59d2c266c8446627f2b52050b84e65ca
Nicholas Charles Emmerson <nickemmerson619@hotmail.com>
2021-11-07 13:39:50 +0000

    Alter webpack to serve correctly, ignore /dist attempt

--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,6 @@ module.exports = {
     },
     mode: 'development',
     devServer: {
-        static: {
-          directory: path.join(__dirname, 'dist'),
-        },
         compress: true,
         port: 9000,
     },
