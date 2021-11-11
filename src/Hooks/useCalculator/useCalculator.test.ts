a5e5ec497d74045809f56a207ac1a2c9cf168218
Nick Emmerson <nickemmerson619@hotmail.com>
2021-11-11 01:48:36 +0000

    Finish writing up useCalculator test successfully

--- src/Hooks/useCalculator/useCalculator.test.ts
+++ src/Hooks/useCalculator/useCalculator.test.ts
@@ -1,43 +1,60 @@
 import { CalculatorState, useCalculator } from './useCalculator';
 import { act, renderHook } from '@testing-library/react-hooks';
 
+const testInputData = [
+    ['8', {     current: CalculatorState.INT_PRESSED_FIRST,     depth: 0,       output: '8',                                    valid: true,        value: 8,           }],
+    ['+', {     current: CalculatorState.OP_PRESSED,            depth: 0,       output: '8 +',                                  valid: false,       value: 8,           }],
+    ['3', {     current: CalculatorState.INT_PRESSED_OTHER,     depth: 0,       output: '8 + 3',                                valid: true,        value: 11,          }],
+    ['x', {     current: CalculatorState.OP_PRESSED,            depth: 0,       output: '8 + 3 x',                              valid: false,       value: 11,          }],
+    ['6', {     current: CalculatorState.INT_PRESSED_OTHER,     depth: 0,       output: '8 + 3 x 6',                            valid: true,        value: 66,          }],
+    ['+', {     current: CalculatorState.OP_PRESSED,            depth: 0,       output: '8 + 3 x 6 +',                          valid: false,       value: 66,          }],
+    ['(', {     current: CalculatorState.START_FIRST,           depth: 1,       output: '8 + 3 x 6 + (',                        valid: false,       value: 66,          }],
+    ['1', {     current: CalculatorState.INT_PRESSED_FIRST,     depth: 1,       output: '8 + 3 x 6 + ( 1',                      valid: false,       value: 66,          }],
+    ['+', {     current: CalculatorState.OP_PRESSED,            depth: 1,       output: '8 + 3 x 6 + ( 1 +',                    valid: false,       value: 66,          }],
+    ['(', {     current: CalculatorState.START_FIRST,           depth: 2,       output: '8 + 3 x 6 + ( 1 + (',                  valid: false,       value: 66,          }],
+    ['3', {     current: CalculatorState.INT_PRESSED_FIRST,     depth: 2,       output: '8 + 3 x 6 + ( 1 + ( 3',                valid: false,       value: 66,          }],
+    ['x', {     current: CalculatorState.OP_PRESSED,            depth: 2,       output: '8 + 3 x 6 + ( 1 + ( 3 x',              valid: false,       value: 66,          }],
+    ['6', {     current: CalculatorState.INT_PRESSED_OTHER,     depth: 2,       output: '8 + 3 x 6 + ( 1 + ( 3 x 6',            valid: false,       value: 66,          }],
+    [')', {     current: CalculatorState.END_FIRST,             depth: 1,       output: '8 + 3 x 6 + ( 1 + ( 3 x 6 )',          valid: false,       value: 66,          }],
+    ['+', {     current: CalculatorState.OP_PRESSED,            depth: 1,       output: '8 + 3 x 6 + ( 1 + ( 3 x 6 ) +',        valid: false,       value: 66,          }],
+    ['5', {     current: CalculatorState.INT_PRESSED_OTHER,     depth: 1,       output: '8 + 3 x 6 + ( 1 + ( 3 x 6 ) + 5',      valid: false,       value: 66,          }],
+    [')', {     current: CalculatorState.END_FIRST,             depth: 0,       output: '8 + 3 x 6 + ( 1 + ( 3 x 6 ) + 5 )',    valid: true,        value: 90,          }]
+];
+
 describe('useCalculator hook', () => {
-
-    let testResult = null;
-    let testWaitForNextUpdate = null;
-
-    beforeAll(() => {
-        const { result, waitForNextUpdate } = renderHook(() => useCalculator());
-        testResult = result;
-        testWaitForNextUpdate = waitForNextUpdate;
-    });
-
-    it.each([
-        ['8', {     current: CalculatorState.INT_PRESSED_FIRST,     depth: 0,       output: '8',                    valid: true,       value: 8,       }],
-        ['+', {     current: CalculatorState.OP_PRESSED,            depth: 0,       output: '8 +',                  valid: false,      value: 8,       }],
-        ['3', {     current: CalculatorState.INT_PRESSED_OTHER,     depth: 0,       output: '',                     valid: true,       value: 11,       }],
-        ['x', {     current: CalculatorState.OP_PRESSED,            depth: 0,       output: '',                     valid: false,       value: 11,       }],
-        ['6', {     current: CalculatorState.INT_PRESSED_OTHER,     depth: 0,       output: '',                     valid: true,       value: 66,       }],
-        ['+', {     current: CalculatorState.OP_PRESSED,            depth: 0,       output: '',                     valid: false,       value: 66,       }],
-        ['(', {     current: CalculatorState.START_FIRST,           depth: 1,       output: '',                     valid: false,       value: 66,       }],
-        ['1', {     current: CalculatorState.INT_PRESSED_FIRST,     depth: 1,       output: '',                     valid: false,       value: 66,       }],
-        ['+', {     current: CalculatorState.OP_PRESSED,            depth: 1,       output: '',                     valid: false,       value:  66,       }],
-        ['(', {     current: CalculatorState.START_FIRST,           depth: 2,       output: '',                     valid: false,       value: 66,       }],
-        ['3', {     current: CalculatorState.INT_PRESSED_FIRST,     depth: 2,       output: '',                     valid: false,       value: 66,       }],
-        ['x', {     current: CalculatorState.OP_PRESSED,            depth: 2,       output: '',                     valid: false,       value: 66,       }],
-        ['6', {     current: CalculatorState.INT_PRESSED_OTHER,     depth: 2,       output: '',                     valid: false,       value: 66,       }],
-        [')', {     current: CalculatorState.INT_PRESSED_FIRST,     depth: 1,       output: '',                     valid: false,       value: 66,       }],
-        ['+', {     current: CalculatorState.OP_PRESSED,            depth: 1,       output: '',                     valid: false,       value: 66,       }],
-        ['5', {     current: CalculatorState.INT_PRESSED_OTHER,     depth: 1,       output: '',                     valid: false,       value: 66,       }],
-        [')', {     current: CalculatorState.END_FIRST,             depth: 0,       output: '',                     valid: true,       value: 90,       }]
-    ])('Test here %#', (arg0, arg1) => {
-
-        const parseArg0 = parseInt(arg0);
-        const input = isNaN(parseArg0) ? arg0 : parseArg0;
+    
+    it('produces correct output for each step of the test equation', () => {
         
-        act(() => { testResult.current.onPress(input); });
+        const { result } = renderHook(() => useCalculator());
 
-        expect(testResult.current).toEqual(expect.objectContaining(arg1));
+        expect(result.current).toEqual(expect.objectContaining({
+            current: CalculatorState.START_FIRST,
+            depth: 0,
+            output: '',
+            valid: false,
+            value: 0,
+        }));
+
+        const enterInputAndVerify = (input, expected) => {
+            const parseInput = parseInt(input);
+            const useInput = isNaN(parseInput) ? input : parseInput;
+            act(() => { result.current.onPress(useInput); });
+            expect(result.current).toEqual(expect.objectContaining(expected));
+        };
+
+        for (const i of testInputData) {
+            enterInputAndVerify(i[0], i[1]);
+        }
+
+        act(() => { result.current.onReset(); });
+
+        expect(result.current).toEqual(expect.objectContaining({
+            current: CalculatorState.START_FIRST,
+            depth: 0,
+            output: '',
+            valid: false,
+            value: 0,
+        }));
 
     });
         
