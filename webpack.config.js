46c823a23c3896d9d4fde771f94a42ade564cfeb
Nick Emmerson <nickemmerson619@hotmail.com>
2023-01-23 15:14:29 +0000

    Fix dependency issues to get running again

--- webpack.config.js
+++ webpack.config.js
@@ -25,5 +25,14 @@ module.exports = {
     plugins: [new HtmlWebpackPlugin({
         title: 'Blindfold Calculator',
         template: 'src/index.html',
-    })]
+    })],
+    /* 
+     * Tried to build with node v18.9.1, npm v8.19.1
+     * Encountered the error seen at this issue with webpack
+     * See comment for fix recommended by webpack author 
+     * https://github.com/webpack/webpack/issues/14532#issuecomment-947525539
+     */
+    output: {
+        hashFunction: "xxhash64",
+    },
 }
